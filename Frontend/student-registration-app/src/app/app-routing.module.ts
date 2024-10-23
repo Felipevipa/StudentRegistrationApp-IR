a7da7e1b8bba7a52637c0b07ca9b89415acb247e
Felipevipa <felipevipa@outlook.com>
2024-10-23 00:25:45 -0500

    Added routes and necessary components

--- Frontend/student-registration-app/src/app/app-routing.module.ts
+++ Frontend/student-registration-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'register', component: StudentRegistrationComponent },
+  { path: 'courses', component: CourseListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/student-registration-app/src/app/app.module.ts
+++ Frontend/student-registration-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentRegistrationComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
