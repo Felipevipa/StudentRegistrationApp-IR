661db36fd850ec409cf8bc3b001031fc5f8b8866
Felipevipa <felipevipa@outlook.com>
2024-10-23 00:00:31 -0500

    Routes configured

--- Frontend/student-registration/src/app/app.routes.ts
+++ Frontend/student-registration/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
+import { CourseListComponent } from './course-list/course-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/register', pathMatch: 'full' },
+    { path: 'register', component: StudentRegistrationComponent },
+    { path: 'courses', component: CourseListComponent }
+  ];
