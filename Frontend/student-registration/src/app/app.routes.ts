b12f7d9cbdc9775e341c7f0af25aeebdd2637aba
Felipevipa <felipevipa@outlook.com>
2024-10-23 00:21:55 -0500

    angular recreated with no standalone components

--- Frontend/student-registration/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { StudentRegistrationComponent } from './student-registration/student-registration.component';
-import { CourseListComponent } from './course-list/course-list.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/register', pathMatch: 'full' },
-    { path: 'register', component: StudentRegistrationComponent },
-    { path: 'courses', component: CourseListComponent }
-  ];
